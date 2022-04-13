e697540aeed2b1056a0f85af212e18aba0a08e3f
Be-student <presentsong@naver.com>
2022-04-13 21:41:01 +0900

    testing...
    asdf

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,8 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @Get('working')
+  getWORKING(): string {
+    return this.appService.getWORKING();
+  }
 }
