1349827c7d908e685c8bc138797d571dca615cb6
khsexk <kohyunsuk98@gmail.com>
2022-04-26 21:13:47 +0900

    socket 구성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RoomsModule } from './rooms/rooms.module';
+import { MultiModule } from './multi/multi.module';
 
 @Module({
-  imports: [],
+  imports: [RoomsModule, MultiModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/multi/adapter/socket-io.adapter.ts
@@ -0,0 +1,10 @@
+import { IoAdapter } from "@nestjs/platform-socket.io";
+
+// Websocket으로 서버가 생성된 부분을 Socket.io 서버로 변경
+export class SocketIoAdapter extends IoAdapter {
+    createIOServer(port: number, options?: any): any {
+        const server = super.createIOServer(port, options);
+
+        return server;
+    }
+}
