5aae443d591075dfed55892ae8602d6136fd1dba
Be-student <presentsong@naver.com>
2022-05-10 01:03:38 +0900

    commit

--- src/rooms/room.repository.ts
+++ src/rooms/room.repository.ts
@@ -1,35 +1,35 @@
-import { Entity, EntityRepository, Repository } from "typeorm";
-import { RoomStatus } from "./room-status.enum";
-import { Room } from "src/entities/Rooms";
-import { CreateRoomDto } from "./dto/room-create.dto";
+import { Entity, EntityRepository, Repository } from 'typeorm';
+import { RoomStatus } from './room-status.enum';
+import { Room } from 'src/entities/Room';
+import { CreateRoomDto } from './dto/room-create.dto';
 
 @EntityRepository(Room)
 export class RoomRepository extends Repository<Room> {
-    async createPrivateRoom(createRoomDto: CreateRoomDto): Promise<Room> {
-        const { roomid, host } = createRoomDto;
+  async createPrivateRoom(createRoomDto: CreateRoomDto): Promise<Room> {
+    const { roomid, host } = createRoomDto;
 
-        const room = this.create({
-            roomid,
-            host,
-            headCount: 1,
-            status: RoomStatus.FRIENDS,
-        });
+    const room = this.create({
+      roomid,
+      host,
+      headCount: 1,
+      status: RoomStatus.FRIENDS,
+    });
 
-        await this.save(room);
-        return room;
-    }
+    await this.save(room);
+    return room;
+  }
 
-    async createPublicRoom(createRoomDto: CreateRoomDto): Promise<Room> {
-        const { roomid, host } = createRoomDto;
+  async createPublicRoom(createRoomDto: CreateRoomDto): Promise<Room> {
+    const { roomid, host } = createRoomDto;
 
-        const room = this.create({
-            roomid,
-            host,
-            headCount: 1,
-            status: RoomStatus.RANDOM,
-        });
+    const room = this.create({
+      roomid,
+      host,
+      headCount: 1,
+      status: RoomStatus.RANDOM,
+    });
 
-        await this.save(room);
-        return room;
-    }
+    await this.save(room);
+    return room;
+  }
 }
