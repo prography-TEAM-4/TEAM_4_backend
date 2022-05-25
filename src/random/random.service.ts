6f8bfcfa87749e51cbc4103e4f5d65321584d855
khsexk <kohyunsuk98@gmail.com>
2022-05-25 19:51:55 +0900

    랜덤모드 구현중
    - 매칭 혹은 방 만들기 구현 (60 타이머 매칭은 아직)
    - 방 입장 api 구현 (친구모드와 유사)
    - api 명세

--- src/random/random.service.ts
+++ src/random/random.service.ts
@@ -30,69 +30,24 @@ export class RandomService {
         private readonly config: ConfigService,
     ){}
 
-    async createOrMatch(token: any, nick: string){
+    async createOrMatch(){
         // 들어갈 방이 있을 때 들어갈 방 리턴
         const matchRoom = await this.findRoom();
-        console.log(matchRoom)
+
         if(matchRoom != null){
             return { result: 'matching success', matchRoom };
         }
 
-        let userData: jwtParsed;
-        let flag: boolean = true;
-
-        try {
-            userData = jwt.verify(token, this.config.get('secret'));
-        } catch (error) {
-            flag = false;
-        } finally {
-            if (flag) {
-                const existUser = await this.userRepository.findOne({
-                    where: {
-                        SnsId: userData.id,
-                        Provider: userData.provider,
-                    },
-                });
+        const roomid: string = v4();
         
-                // 로그인 한 경우 가능
-                if (existUser) {
-                    const roomid: string = v4();
+        const room = new Room();
+        room.roomid = roomid;
+        room.host = 'RANDOM';
+        room.headCount = 0;
+        room.status = 'RANDOM';
+        await this.randomRoomRepository.save(room);
         
-                    const room = new Room();
-                    room.roomid = roomid;
-                    room.host = existUser.SnsId;
-                    room.headCount = 0;
-                    room.status = 'RANDOM';
-                    await this.randomRoomRepository.save(room);
-                    return { result: 'create success', room };
-                }
-            }
-
-            else {
-                // 중복 닉네임 예외처리
-                const duplicate_check = await this.memberRepository.findOne({
-                    where: { Nick: nick },
-                });
-  
-                if (duplicate_check) {
-                    throw new HttpException('Duplicated Nickname', HttpStatus.BAD_REQUEST);
-                }
-
-                const roomid: string = v4();
-        
-                const room = new Room();
-                room.roomid = roomid;
-                room.host = nick;
-                room.headCount = 0;
-                room.status = 'RANDOM';
-                await this.randomRoomRepository.save(room);
-        
-                const member = new Member();
-                member.Nick = nick;
-                await this.memberRepository.save(member);
-                return { result: 'create success', room };
-            }
-        }
+        return { result: 'create success', room };
     }
 
     async findRoom(){
@@ -108,6 +63,83 @@ export class RandomService {
         } else{
             return null;
         }
+    }
 
+    async enterRoom(
+        roomid: string, 
+        token: any, 
+        nick: string, 
+        imgCode: string
+    ){
+        let userData: jwtParsed;
+        let flag: boolean = true;
+
+        try {
+            userData = jwt.verify(token, this.config.get('secret'));
+        } catch (error) {
+            flag = false;
+        } finally {
+            const room = await this.randomRoomRepository.findOne({
+                where: { roomid: roomid },
+            });
+
+            // 없는 방이거나 6명 이상인 경우
+            if (!room || room.headCount >= 6) {
+                throw new HttpException('Not Exist Room', HttpStatus.NOT_FOUND);
+            }
+
+            if (flag) {
+                const existUser = await this.userRepository.findOne({
+                    where: {
+                        SnsId: userData.id,
+                        Provider: userData.provider,
+                    },
+                });
+                if (existUser) {
+                    existUser.room = room;
+                    existUser.all = imgCode;
+                    await this.userRepository.save(existUser);
+                }
+                else{
+                    flag = false
+                }
+            }
+
+            // 로그인을 하지 않은 유저
+            if(!flag){
+                const duplicate_check = await this.memberRepository.findOne({
+                    where: { Nick: nick },
+                });
+
+                if (duplicate_check) {
+                    throw new HttpException('Duplicated Nickname', HttpStatus.BAD_REQUEST);
+                }
+
+                const member = new Member();
+                member.Nick = nick;
+                member.room = room;
+                member.all = imgCode;
+                await this.memberRepository.save(member);
+            }
+
+            const memberList: Array<Member> = await this.memberRepository
+                .createQueryBuilder('members')
+                .innerJoin('members.room', 'room', 'room.roomid = :roomid', {
+                    roomid,
+                })
+                .getMany();
+
+            room.headCount += 1;
+            await this.randomRoomRepository.save(room);
+
+            const userList: Array<Member> = await this.userRepository
+                .createQueryBuilder('users')
+                .innerJoin('users.room', 'room', 'room.roomid = :roomid', {
+                    roomid,
+                })
+                .getMany();
+
+            return { userList, memberList, room };
+        }
     }
 }
