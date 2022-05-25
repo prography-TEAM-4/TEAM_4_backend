f5c55a6af2c9a6798f3cdaab74428f55ec295e19
khsexk <kohyunsuk98@gmail.com>
2022-05-25 18:02:22 +0900

    랜덤 매칭 구현

--- src/random/random.controller.ts
+++ src/random/random.controller.ts
@@ -1,4 +1,81 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Headers } from '@nestjs/common';
+import { ApiBody, ApiHeader, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { RandomService } from './random.service';
 
-@Controller('random')
-export class RandomController {}
+@ApiTags('Multi - Random')
+@Controller('mode/random')
+export class RandomController {
+    constructor(private randomService: RandomService){}
+
+    // @Get()
+    // getRandomRoomList(){
+
+    // }
+
+    @ApiHeader({
+        name: 'Authorization',
+        description: 'eyJhGcioJ와 같은 accessToken',
+    })
+    @ApiBody({
+        description: '비로그인 유저는 닉네임, 로그인 유저는 null',
+        schema: {
+            example: { nick: 'example' },
+        },
+    })
+    @ApiResponse({
+        description: '로그인 유저: 랜덤방 만들기 성공',
+        status: 200,
+        schema: {
+            example: {
+                result: 'create success',
+                room: [{
+                    id: 'id', 
+                    roomid: 'b7817821-dc63-4f09-97cc-32d466701077', 
+                    host: 'user@example.com', 
+                    headCount: '0', 
+                    status: 'RANDOM', 
+                }],
+            },
+        },
+    })
+    @ApiResponse({
+        description: '비로그인 유저: 랜덤방 만들기 성공',
+        status: 200,
+        schema: {
+            example: {
+                result: 'create success',
+                room: [{
+                    id: 'id', 
+                    roomid: 'b7817821-dc63-4f09-97cc-32d466701077', 
+                    host: 'member1', 
+                    headCount: '0', 
+                    status: 'RANDOM', 
+                }],
+            },
+        },
+    })
+    @ApiResponse({
+        description: '매칭 성공',
+        status: 200,
+        schema: {
+            example: {
+                result: 'matching success',
+                room: [{
+                    id: 'id', 
+                    roomid: 'b7817821-dc63-4f09-97cc-32d466701077', 
+                    host: 'member1', 
+                    headCount: '5', 
+                    status: 'RANDOM', 
+                }],
+            },
+        },
+    })
+    @ApiOperation({ summary: '랜덤매칭' })
+    @Post('')
+    async createOrMatch(
+        @Headers('Authorization') token: any,
+        @Body('nick') nick: string,
+    ){
+        return this.randomService.createOrMatch(token, nick);
+    }
+}
--- src/random/random.service.ts
+++ src/random/random.service.ts
@@ -1,4 +1,113 @@
-import { Injectable } from '@nestjs/common';
+import { 
+    HttpException, 
+    HttpStatus, 
+    Injectable 
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Member } from 'src/entities/Member';
+import { Room } from 'src/entities/Room';
+import { RoomChat } from 'src/entities/RoomChat';
+import { User } from 'src/entities/User';
+import { MultiGateway } from 'src/multi/multi.gateway';
+import { jwtParsed } from 'src/user/dto/userdata.dto';
+import { Not, Repository } from 'typeorm';
+import * as jwt from 'jsonwebtoken';
+import { v4 } from 'uuid';
 
 @Injectable()
-export class RandomService {}
+export class RandomService {
+    constructor(
+        @InjectRepository(Room)
+        private randomRoomRepository: Repository<Room>,
+        @InjectRepository(RoomChat)
+        private randomRoomChatRepository: Repository<RoomChat>,
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
+        @InjectRepository(Member)
+        private memberRepository: Repository<Member>,
+        private readonly multiGatway: MultiGateway,
+        private readonly config: ConfigService,
+    ){}
+
+    async createOrMatch(token: any, nick: string){
+        // 들어갈 방이 있을 때 들어갈 방 리턴
+        const matchRoom = await this.findRoom();
+        console.log(matchRoom)
+        if(matchRoom != null){
+            return { result: 'matching success', matchRoom };
+        }
+
+        let userData: jwtParsed;
+        let flag: boolean = true;
+
+        try {
+            userData = jwt.verify(token, this.config.get('secret'));
+        } catch (error) {
+            flag = false;
+        } finally {
+            if (flag) {
+                const existUser = await this.userRepository.findOne({
+                    where: {
+                        SnsId: userData.id,
+                        Provider: userData.provider,
+                    },
+                });
+        
+                // 로그인 한 경우 가능
+                if (existUser) {
+                    const roomid: string = v4();
+        
+                    const room = new Room();
+                    room.roomid = roomid;
+                    room.host = existUser.SnsId;
+                    room.headCount = 0;
+                    room.status = 'RANDOM';
+                    await this.randomRoomRepository.save(room);
+                    return { result: 'create success', room };
+                }
+            }
+
+            else {
+                // 중복 닉네임 예외처리
+                const duplicate_check = await this.memberRepository.findOne({
+                    where: { Nick: nick },
+                });
+  
+                if (duplicate_check) {
+                    throw new HttpException('Duplicated Nickname', HttpStatus.BAD_REQUEST);
+                }
+
+                const roomid: string = v4();
+        
+                const room = new Room();
+                room.roomid = roomid;
+                room.host = nick;
+                room.headCount = 0;
+                room.status = 'RANDOM';
+                await this.randomRoomRepository.save(room);
+        
+                const member = new Member();
+                member.Nick = nick;
+                await this.memberRepository.save(member);
+                return { result: 'create success', room };
+            }
+        }
+    }
+
+    async findRoom(){
+        const room = await this.randomRoomRepository.findOne({
+            where: {
+                status: 'RANDOM',
+                headCount: Not(6),
+            },
+        });
+
+        if(room){
+            return room;
+        } else{
+            return null;
+        }
+
+    }
+}
