88670b25b1e27058ed5c351ddb06aeeb157b6694
khsexk <kohyunsuk98@gmail.com>
2022-06-05 15:32:48 +0900

    leave 로그 추가

--- src/multi/multi.gateway.ts
+++ src/multi/multi.gateway.ts
@@ -89,5 +89,9 @@ export class MultiGateway
     delete ConnectedUsers[client.nsp.name][client.id];
     console.log(ConnectedUsers);
     nsp.emit('connectedList', Object.values(ConnectedUsers[client.nsp.name]));
+
+    this.server.sockets.adapter.on('leave-room', (room, id) => {
+      console.log(`socket ${id} has leaved room ${room}`);
+    });
   }
 }
