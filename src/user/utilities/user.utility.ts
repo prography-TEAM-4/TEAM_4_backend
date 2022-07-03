f3ed1a142f77c5a5e85c5448a379a960ed517871
Be-student <presentsong@naver.com>
2022-07-04 00:37:41 +0900

    shorten random Nick geneator

--- src/user/utilities/user.utility.ts
+++ src/user/utilities/user.utility.ts
@@ -1,37 +1,32 @@
-export const getRandomNickname=(animalCode:number)=>{
-    const first=[
-        "용감한 ",
-        "공부하기 싫은 ",
-        "게으른 ",
-        "열정적인 ",
-        "공부가 좋은 ",
-        "놀고 싶은 ",
-        "행복한 ",
-        "귀찮은 ",
-        "즐거운 ",
-        "혼자있고 싶은 ",
-        "수업듣기 싫은 ",
-    ];
-    const second=[
-        "푸른 ",
-        "붉은 ",
-        "분홍 ",
-        "초록 ",
-        "흰색 ",
-        "검은색 ",
-        "노랑 ",
-        "초콜릿색 ", 
-    ];
-    const third=[
-        "",
-        "고양이",
-        "소",
-        "도마뱀",
-        "강아지",
-        "돼지",
-        "토끼"
-    ]
-    const randomCode= Math.random();
-    return first[Math.floor(randomCode*first.length)]+second[Math.floor(randomCode*second.length)]+third[animalCode];
-
-}
+export const getRandomNickname = (animalCode: number) => {
+  const first = [
+    '용감한 ',
+    '졸린 ',
+    '게으른 ',
+    '열정적인 ',
+    '지루한 ',
+    '아픈 ',
+    '행복한 ',
+    '귀찮은 ',
+    '즐거운 ',
+    '슬픈 ',
+    '나른한 ',
+  ];
+  const second = [
+    '푸른 ',
+    '붉은 ',
+    '분홍 ',
+    '초록 ',
+    '흰색 ',
+    '검은색 ',
+    '노랑 ',
+    '초콜릿색 ',
+  ];
+  const third = ['', '고양이', '소', '도마뱀', '강아지', '돼지', '토끼'];
+  const randomCode = Math.random();
+  return (
+    first[Math.floor(randomCode * first.length)] +
+    second[Math.floor(randomCode * second.length)] +
+    third[animalCode]
+  );
+};
