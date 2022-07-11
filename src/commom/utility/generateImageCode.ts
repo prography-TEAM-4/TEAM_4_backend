666c0a3f2c0cfedf206ae5d55632b1ce41268eac
khsexk <kohyunsuk98@gmail.com>
2022-07-11 22:43:11 +0900

    refac sort 옵션 변경

--- src/commom/utility/generateImageCode.ts
+++ src/commom/utility/generateImageCode.ts
@@ -6,44 +6,18 @@ export function generateImageCode(userList: number[]): string {
     return `${userList[0]}_${userList[0]}_${userList[0]}_${userList[0]}_${userList[0]}_${userList[0]}`;  
   }
 
-  // 2명 이상일 때
-  const imgList: UserImage[] = [];
-
-  userList.forEach((imgCode)=> {
-    imgList.push(new UserImage(imgCode, Math.round(Math.random() * (100-1) + 1)));
-  });
-
-  while(imgList.length !== 6){
+  while(userList.length !== 6){
     const randomNum: number = Math.round(Math.random() * (len-1) + 1);
-    imgList.push(new UserImage(userList[randomNum-1], Math.round(Math.random() * (100-1) + 1)));
+    userList.push(userList[randomNum-1]);
   }
 
-  const randomSort: any = (x: UserImage, y: UserImage) => {
-    const result: number = x.priority - y.priority;
-
-    if(result == 0){
-      return Math.round(Math.random() * (100-1) + 1) - Math.round(Math.random() * (100-1) + 1);
-    }
-    return result;
-  }
-
-  imgList.sort(randomSort);
+  userList.sort(() => Math.random() - 0.5);
 
   let mixImg: string = '';
 
-  imgList.forEach((img) => {
-    mixImg += (img.data + '_');
+  userList.forEach((img) => {
+    mixImg += (img + '_');
   })
 
   return mixImg.substring(0, 11);
-}
-
-class UserImage {
-  data: number;
-  priority: number;
-
-  constructor(data: number, priority:number){
-    this.data = data;
-    this.priority = priority;
-  }
 }
