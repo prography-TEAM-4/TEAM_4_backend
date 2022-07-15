87e82c7d6b9bcf461816a87726eb368193089563
Be-student <presentsong@naver.com>
2022-07-16 01:10:17 +0900

    안 쓰는 부분 삭제

--- src/multi/multi.module.ts
+++ src/multi/multi.module.ts
@@ -5,11 +5,10 @@ import { Room } from 'src/entities/Room';
 import { RoomChat } from 'src/entities/RoomChat';
 import { User } from 'src/entities/User';
 import { MultiGateway } from './multi.gateway';
-import { MultiService } from './multi.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Member, Room, RoomChat])],
-  providers: [MultiGateway, MultiService],
+  providers: [MultiGateway],
   exports: [MultiGateway],
 })
 export class MultiModule {}
